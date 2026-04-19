ed4ed5470afbc09e4b04cc05c2229b5b0bb65802
Harshita Gupta <jai.mahakal156213@gmail.com>
2026-04-19 09:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-
 #=============================================================
 #                         ALGORITHM
 #=============================================================
